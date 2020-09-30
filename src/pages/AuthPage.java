79bf2ba9e2f1ed93516ed169713d92b7798951bc
Natalia Peychev <natkebatke@gmail.com>
2020-09-30 21:39:02 +0200

    Meal and Auth pages finished

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AuthPage extends BasicPage {
@@ -9,7 +11,22 @@ public class AuthPage extends BasicPage {
 	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
 		super(driver, js, waiter);
 	}
-	
-	
+
+	public WebElement getUserMenu() {
+		return this.driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div[2]/div[2]/ul/li/a"));
+	}
+
+	public WebElement getMyAccountBtn() {
+		return this.driver.findElement(By.xpath("//a[@href='/member']"));
+	}
+
+	public WebElement getLogoutBtn() {
+		return this.driver.findElement(By.xpath("//a[@href='/guest-user/logout']"));
+	}
+
+	public void userLogout() {
+		getUserMenu().click();
+		getLogoutBtn().click();
+	}
 
 }
--- src/pages/MealPage.java
+++ src/pages/MealPage.java
@@ -1,7 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MealPage extends BasicPage {
@@ -9,7 +12,27 @@ public class MealPage extends BasicPage {
 	public MealPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
 		super(driver, js, waiter);
 	}
-	
-	
+
+	public WebElement getFavouriteBtn() {
+		return this.driver.findElement(By.xpath("//*[contains(@class, 'favourite')]"));
+	}
+
+	public WebElement getQuantityInput() {
+		return this.driver.findElement(By.xpath("//input[@type='number']"));
+	}
+
+	public WebElement getAdd2CartBtn() {
+		return this.driver.findElement(By.xpath("//*[contains(@class, 'btn--primary')]"));
+	}
+
+	public void addMeal2cart(int quantity) {
+		getQuantityInput().sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		getQuantityInput().sendKeys(quantity + "");
+		getAdd2CartBtn().click();
+	}
+
+	public void addMeal2favourites() {
+		getFavouriteBtn().click();
+	}
 
 }
