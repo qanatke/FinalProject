c46c0a3bb4b0ebb6e13de257208616b5b6179674
Natalia Peychev <natkebatke@gmail.com>
2020-09-30 21:55:11 +0200

    CartSummaryPage finished

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CartSummaryPage extends BasicPage {
@@ -9,7 +11,13 @@ public class CartSummaryPage extends BasicPage {
 	public CartSummaryPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
 		super(driver, js, waiter);
 	}
-	
-	
+
+	public WebElement getClearAllBtn() {
+		return this.driver.findElement(By.xpath("//div[@class='cart-head']/a[2]"));
+	}
+
+	public void clearAll() {
+		getClearAllBtn().click();
+	}
 
 }
