331b55207b12882897c8b81ee49047d0b018138c
Natalia Peychev <natkebatke@gmail.com>
2020-10-01 21:57:14 +0200

    Search results pafe and test added

--- src/pages/SearchResultPage.java
+++ src/pages/SearchResultPage.java
@@ -1,7 +1,12 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchResultPage extends BasicPage {
@@ -10,5 +15,19 @@ public class SearchResultPage extends BasicPage {
 		super(driver, js, waiter);
 	}
 
-	
+	public List<WebElement> searchResults() {
+		return this.driver.findElements(By.xpath("//*[@class='product-name']/a"));
+	}
+
+	public List<String> mealNames() {
+		List<String> mealsFound = new ArrayList<>();
+		for (int i = 0; i < searchResults().size(); i++) {
+			mealsFound.add(searchResults().get(i).getText());
+		}
+		return mealsFound;
+	}
+
+	public int mealsCount() {
+		return searchResults().size();
+	}
 }
--- src/tests/SearchTest.java
+++ src/tests/SearchTest.java
@@ -1,5 +1,58 @@
 package tests;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.sun.rowset.internal.Row;
+
+import pages.LocationPopupPage;
+import pages.MealPage;
+import pages.SearchResultPage;
+
 public class SearchTest extends BasicTest {
 
+	@Test
+	public void searchResultsTest() throws Exception {
+
+		LocationPopupPage locationPopup = new LocationPopupPage(this.driver, this.js, this.waiter);
+		MealPage mealPage = new MealPage(this.driver, this.js, this.waiter);
+		SearchResultPage searchResults = new SearchResultPage(this.driver, this.js, this.waiter);
+
+		File meals = new File("data/Data.xlsx");
+		FileInputStream fis = new FileInputStream(meals);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet mealsSearchheet = wb.getSheet("Meal Search Results");
+
+		this.driver.navigate().to(baseUrl + "/meals");
+
+		locationPopup.setLocation(locationName);
+		// waiting for synchronization
+		Thread.sleep(2000);
+
+		for (int i = 1; i <= mealsSearchheet.getLastRowNum(); i++) {
+			XSSFRow row = mealsSearchheet.getRow(i);
+
+			String mealUrl = row.getCell(1).getStringCellValue();
+			// waiting for synchronization
+			Thread.sleep(2000);
+			this.driver.navigate().to(mealUrl);
+
+			locationPopup.getLocationForm().click();
+			locationPopup.setLocation(row.getCell(0).getStringCellValue());
+
+			softAssert.assertEquals(searchResults.mealsCount(), row.getCell(2).getNumericCellValue());
+
+			for (int j = 3; j < mealsSearchheet.getRow(i).getCell(2).getNumericCellValue() + 3; j++) {
+				String expected = mealsSearchheet.getRow(i).getCell(j).getStringCellValue();
+				String actual = searchResults.mealNames().get(j - 3);
+				softAssert.assertEquals(actual, expected, " [ERROR] Meal does not exist");
+			}
+		}
+	}
 }
